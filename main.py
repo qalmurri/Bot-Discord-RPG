643a0abf9f6546335cfbcf6518b5295d298d2ce4
qalmurri <86642474+qalmurri@users.noreply.github.com>
2023-10-16 10:28:13 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix=".", intents=discord.Intents.all())
+
+@bot.event
+async def on_ready():
+    await bot.tree.sync()
+    print(f'Base {bot.user.name} ({bot.user.id}) on {len(bot.guilds)} Server, Online!')
+
+@bot.event
+async def setup_hook() -> None:
+    extensions = [
+        'cogs.activity.message',
+        'cogs.commands.profile',
+        'cogs.commands.join',
+        'rpg.commands.equip'
+        ]
+    
+    for extension in extensions:
+        await bot.load_extension(extension)
+
+bot.run("OTY3MTcwODYxNTA3NDQwNjUw.Gt_s_3.ptw1JtxeYERmeoALBXiCwyPlPIIZ9c__boRBPY")
